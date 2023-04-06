38926e921587963e76c16194ca63dfc627a96881
Sarah Lauser <sarah@sarahlauser.com>
2023-04-05 21:51:20 -0400

    tested hypotheses 1 and 2

--- loadData.R
+++ loadData.R
@@ -85,7 +85,81 @@ bookSummaries %>% filter(!is.na(nameMaleness)) %>% nrow() # 11098
 
 bookSummaries %>% filter(!is.na(nameMaleness)) %>% summarize(meanMaleness = mean(nameMaleness)) # 0.713
 
+# add a authorSex column to the bookSummaries dataframe
+bookSummaries$authorSex <- case_when(bookSummaries$nameMaleness >= 0.60 ~ "Male",
+                                     bookSummaries$nameFemaleness >= 0.60 ~ "Female",
+                                     TRUE ~ "Indeterminate")
+
+# plot maleness vs femaleness
+ggplot(bookSummaries, aes(x = authorSex)) + 
+  geom_bar() +
+  geom_text(aes(label = ..count..), stat = "count", vjust = 1.5, colour = "white")+
+  labs(title = "Corpus authorship by sex") +
+  theme(
+    panel.background = element_rect(fill='transparent', color=NA), #transparent panel bg - remove color tag for border
+    plot.background = element_rect(fill='transparent', color=NA), #transparent plot bg
+    panel.grid.major.y = element_line(color = "grey60"), # leave y major gridlines
+    panel.grid.minor = element_blank(), #remove minor gridlines
+    legend.background = element_rect(fill='transparent'), #transparent legend bg
+    legend.box.background = element_rect(fill='transparent') #transparent legend panel
+  )
+
 # add a rough word count
 bookSummaries$summaryWordCount <- str_count(bookSummaries$plotSummary, '\\w+')
 
-bookSummaries %>% filter(!is.na(nameMaleness)) %>% summarize(totalWords = sum(summaryWordCount))
+wordCounts <- bookSummaries %>% group_by(authorSex) %>% summarize(meanWords = mean(summaryWordCount))
+
+# plot summary word count by authorSex
+ggplot(wordCounts, aes(x = authorSex, y = meanWords)) + 
+  geom_col() +
+  geom_text(aes(label = meanWords), vjust = 1.5, color = "white")+
+  labs(title = "Average word count of plot summary by author's sex") +
+  theme(
+    panel.background = element_rect(fill='transparent', color=NA), #transparent panel bg - remove color tag for border
+    plot.background = element_rect(fill='transparent', color=NA), #transparent plot bg
+    panel.grid.major.y = element_line(color = "grey60"), # leave y major gridlines
+    panel.grid.minor = element_blank(), #remove minor gridlines
+    legend.background = element_rect(fill='transparent'), #transparent legend bg
+    legend.box.background = element_rect(fill='transparent') #transparent legend panel
+  )
+
+# run a correlation for author sex vs summary word count
+# first, need to make a dummy variable for authorSex
+
+bookSummaries$authorSexInt <- NULL
+
+bookSummaries$authorSexInt <- ifelse(bookSummaries$authorSex == 'Male',1,
+                                     ifelse(bookSummaries$authorSex == 'Female',0,NA))
+
+#-------------------------------------------------------------------------------
+# different correlation testing options
+#-------------------------------------------------------------------------------
+library(psych)
+
+corr.test(bookSummaries$authorSexInt, bookSummaries$summaryWordCount)
+# > corr.test(bookSummaries$authorSexInt, bookSummaries$summaryWordCount)
+# Call:corr.test(x = bookSummaries$authorSexInt, y = bookSummaries$summaryWordCount)
+# Correlation matrix 
+# [1] 0.01            mild positive correlation -- males have more words
+# 
+# Sample Size 
+# [1] 12685
+# 
+# These are the unadjusted probability values.
+# The probability values  adjusted for multiple tests are in the p.adj object. 
+# [1] 0.12            result is not statistically significant
+
+# stats package cor
+cor(bookSummaries[,c("authorSexInt","summaryWordCount")], method = c("pearson"), use = "complete.obs")
+
+# Hmisc for detailed rho and p values
+install.packages("Hmisc")
+library(Hmisc)
+
+res2 <- rcorr(as.matrix(bookSummaries[,c("authorSexInt","summaryWordCount")]))
+
+res2$r
+
+res2$P 
+
+# p = 0.1224107 so there is no statistical significance
